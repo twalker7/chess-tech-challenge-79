09835746f310b69ae1feab392e882feeb2977f5b
Thomas Walker <thomasnwalker@yahoo.com>
2021-11-14 11:18:34 -0800

    testing react elements -- pre delete/refine

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import './App.css';
 import Board from './components/Board';
 
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'; // major key 
+import {Button, Alert, Breadcrumb, Card, Row, Column} from 'react-bootstrap';
 function App() {
   return (
     <div className="App">
       <header> CHESS TECH </header>
       <div className="App-page">
       <div>
-        <Board></Board>
-        <Button className="button"> button 1 </Button> 
-        <Button className="button"> button 2 </Button>
+        <Board> </Board>
+
+      
+        <Alert variant="success"> This is an alert button now </Alert>
+        <Breadcrumb.Item> First </Breadcrumb.Item> 
+        <Breadcrumb.Item> Second </Breadcrumb.Item>
+        <Breadcrumb.Item> Third </Breadcrumb.Item>
+
+      
+
+        <Button className="button"> button1 </Button> 
+        <Button className="button"> button2 </Button>
         </div>
       </div>
       
--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+
+
+import { Card, Button } from 'react-bootstrap';
 function Board(){
     return(
         <div className="board-container"> 
-        Chess Board
-        
+        <h2> Chess Board contents here </h2> 
+        <img alt="chess board" src=""/>
+
+
+        <Card className="mb-3" style={{color:"#000"}} > 
+            <Card.Img src="https://picsum.photos/200"/>
+            <Card.Body>
+              <Card.Title>
+                Console for Chess puzzle stats will go here 
+              </Card.Title>
+            
+              
+              <Button variant="primary "> Puzzle Book Navigation Button </Button> 
+            </Card.Body>
+          </Card>
         </div>
     )
 }
